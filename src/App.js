b68e24a085f49512a88694811b538560c5191dfc
Laércio Oliveira <jose.laercio.bezerra@gmail.com>
2019-05-25 01:10:24 -0300

    Trocar o App de um component statefull para um stateless

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-function App() {
-  return (
-    <h1>Aprendendo React!</h1>
-  );
-}
+// Componente Stateless
+
+const App = () => <h1>Componente Stateless</h1>
 
 export default App;
